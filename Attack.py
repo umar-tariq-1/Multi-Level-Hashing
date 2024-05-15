a28f950c7ffb10c989a078a0b503cb6d75687050
umart823 <umart823@gmail.com>
2024-05-16 01:23:38 +0500

    Attack

--- Attack.py
+++ Attack.py
@@ -13,19 +13,27 @@ def find_second_preimage(hash_function, original_data, hash_value):
     max_attempts = 10000  # Maximum number of attempts to find a second pre-image
     original_hash = hash_function(original_data)
 
+    file_names = ["Attack Files/file1.txt",'Attack Files/file2.txt','Attack Files/file3.txt','Attack Files/file4.txt','Attack Files/file5.txt']
+    
+    for file_name in file_names:
+        encoder = Encoder(file_name, isFile=True)
+        candidate_hash = encoder.getFinalHash()
+        if (candidate_hash == hash_value):
+            return f"\n{file_name} matched!"
+        print(f"Not matched with {file_name}")
+
     for _ in range(max_attempts):
         # Generate a random string of the same length as original_data
         candidate_data = generate_random_string(len(original_data))
-        candidate_hex = create_hex(candidate_data)
 
-        # Calculate the hash of the candidate data
-        candidate_hash = hash_function(candidate_hex)
+        encoder = Encoder(candidate_data, isFile=False)
+        candidate_hash = encoder.getFinalHash()
 
         # Check if the candidate data produces the same hash as the original data
         if candidate_hash == hash_value and candidate_data != original_data:
             return candidate_data
-
-    return "Not found within 10,000 iterations."  # Second pre-image not found within the maximum attempts
+    
+    return "Not matched within 10,000 iterations."  # Second pre-image not found within the maximum attempts
 
 
 def attack()->None:
@@ -48,6 +56,7 @@ def attack()->None:
         print(find_second_preimage(encoder.getHashFunction(),encoder.getOriginalData(),encoder.getFinalHash()))
     else:
         encoder = Encoder(args.string, isFile=False)
+        print(find_second_preimage(encoder.getHashFunction(),encoder.getOriginalData(),encoder.getFinalHash()))
     
     
 attack()
