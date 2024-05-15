bab37e0840d60593c7b019536653277a6993a5ea
umart823 <umart823@gmail.com>
2024-05-16 01:45:07 +0500

    Attack

--- Attack.py
+++ Attack.py
@@ -40,7 +40,7 @@ def attack()->None:
     parser = argparse.ArgumentParser(
         prog="Multi Level Encoder for Hashing",
         description="This program read a file and creates a Merkle Tree ready for Multi-Level Hashing",
-        epilog="Created by \033[1mMuhammad Sunaam :)\033[0m"
+        epilog="Created by \033[1mUmar Tariq :)\033[0m"
     )
     parser.add_argument("-f","--file",action='store',help="stores path of the file you want to hash", type=str)
     parser.add_argument("-s","--string",action='store',help="sstring you want to hash", type=str)
